2457d84a2c8cc22af2f2cd2bb621297d451da8e3
Tomas Ruzicka <tomasruzicka@abdoc.net>
2014-10-15 22:58:14 +0200

    ver 1.2.0

--- dust.js
+++ dust.js
@@ -29,7 +29,7 @@ var helpers = {
     },
     objCFont: function(chunk, ctx, bodies, params) {
       var baseTextStyle = ctx.get('baseTextStyle');
-      return chunk.write(baseTextStyle.font.name);
+      return chunk.write(baseTextStyle.font.postScriptName || baseTextStyle.font.name);
     }
 };
 
